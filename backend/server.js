b22265ff0de4e3f5479a8f2472f6021713f5ccfb
Andriatsimaniry <niryomega@free.fr>
2021-08-17 23:57:53 +0200

    Ajout Api backend

--- backend/server.js
+++ backend/server.js
@@ -29,26 +29,27 @@ db.sequelize.sync().then(() => {
   initial();
 });
 
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user",
-  });
+// function initial() {
+//   Role.create({
+//     id: 1,
+//     name: "user",
+//   });
 
-  Role.create({
-    id: 2,
-    name: "moderator",
-  });
+//   Role.create({
+//     id: 2,
+//     name: "moderator",
+//   });
 
-  Role.create({
-    id: 3,
-    name: "admin",
-  });
-}
+//   Role.create({
+//     id: 3,
+//     name: "admin",
+//   });
+// }
 
 // routes
 require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
+require("./routes/post.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
