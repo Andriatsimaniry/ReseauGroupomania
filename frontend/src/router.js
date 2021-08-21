f39c9bbe4a27d535dedaa4fccb9e1fc6f903d00d
Andriatsimaniry <niryomega@free.fr>
2021-08-21 16:36:19 +0200

    garde url utilisateurs front

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,12 +5,9 @@ import Login from "./components/Login.vue";
 import Register from "./components/Register.vue";
 import UserList from "./components/UserList.vue";
 // lazy-loaded
-const Profile = () => import("./components/Profile.vue")
-
+const Profile = () => import("./components/Profile.vue");
 
 const routes = [
-  
-  
   {
     path: "/login",
     component: Login,
@@ -28,6 +25,13 @@ const routes = [
   {
     path: "/users",
     name: "users",
+    beforeEnter(to, from, next) {
+      if(isAdmin()) {
+        next();
+      } else {
+        next(from.path);
+      }
+    },
     // lazy-loaded
     component: UserList,
   },
@@ -35,10 +39,10 @@ const routes = [
   // Routes Posts
   {
     path: "/", // Chemin de l'Url pour la route Posts
-    alias: "/posts", 
+    alias: "/posts",
     name: "posts", // Nom de la route
-    component: () => import("./components/PostsList") //Composant à charge lorsque cette route est appelée
-  }
+    component: () => import("./components/PostsList"), //Composant à charge lorsque cette route est appelée
+  },
 ];
 
 const router = createRouter({
@@ -46,18 +50,28 @@ const router = createRouter({
   routes,
 });
 
+const isAdmin = function() {
+  const user = localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : null;
+  if (user) {
+    return user.roles.includes("ROLE_ADMIN");
+  }
+  return false;
+};
+
 router.beforeEach((to, from, next) => {
-    const publicPages = ['/login', '/register'];
-    const authRequired = !publicPages.includes(to.path);
-    const loggedIn = localStorage.getItem('user');
-  
-    // trying to access a restricted page + not logged in
-    // redirect to login page
-    if (authRequired && !loggedIn) {
-      next('/login');
-    } else {
-      next();
-    }
-  });
+  const publicPages = ["/login", "/register"];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem("user");
+
+  // trying to access a restricted page + not logged in
+  // redirect to login page
+  if (authRequired && !loggedIn) {
+    next("/login");
+  } else {
+    next();
+  }
+});
 
 export default router;
