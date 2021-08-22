41535b555f6e8127be1f0e042b00bf01e2f0d11c
Andriatsimaniry <niryomega@free.fr>
2021-08-22 11:48:14 +0200

    Sécurité Backend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,38 @@
 const express = require("express");
-const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const app = express();
 
 
-global.__basedir = __dirname;
+//Résolution des Cors
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
-var corsOptions = {
-  origin: "http://localhost:8081",
-};
+//  Pour proteger l'application contre les attaques
+// content-Security-Policy,x-powered-by,Strict-Transport-Security,
+// X-Download-Option,Cache-Control,Content-Type-Options,X-frame-Option,X-Xss-Protection
+app.use(helmet());
 
-app.use(cors(corsOptions));
+
+// Pour éviter les attaques de force brute
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limite chaque IP à 100 requêtes
+});
+
+//  S'applique à toutes les demandes
+app.use("/api/", apiLimiter);
+
 
 // analyser les requêtes de type content - application/json
 app.use(express.json());
